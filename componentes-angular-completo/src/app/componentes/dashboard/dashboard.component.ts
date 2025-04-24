b400a162481ebcee1f59cb9d3c58e6f018fbd9f6
Cleyton Santiago <cleyton.santiago@gmail.com>
2025-04-24 16:56:50 -0300

    ifelse validacao do select

--- componentes-angular-completo/src/app/componentes/dashboard/dashboard.component.ts
+++ componentes-angular-completo/src/app/componentes/dashboard/dashboard.component.ts
@@ -1,38 +1,83 @@
 import { Component, OnInit } from '@angular/core';
 import { ScrollNavBarComponent } from "../scroll-nav-bar/scroll-nav-bar.component";
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [ScrollNavBarComponent],
+  imports: [ScrollNavBarComponent, CommonModule,FormsModule],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css',]
 })
 export class DashboardComponent {
+  inputVeiculo: string = '';
   async ngOnInit() {
     fetch('http://localhost:3000/api/table/VEHICLE').then(async (response) => {
-    const resposta= await response.json(); 
-    this.respostaveiculo = resposta;
-    // console.log(resposta);
-    // if ("input-veiculo" in resposta(2)) {
-    //   console.log(resposta["input-veiculo"]);
-    // }
-    // else {
-    //   console.log("input-veiculo não encontrado");
-    // }
-    console.log(resposta); 
+      const resposta = await response.json();
+      this.respostaveiculo = resposta;
+      
+      console.log(resposta);
+      
+      for (let i = 0; i < resposta.length; i++) {
+        console.log(this.inputVeiculo)
+        console.log(resposta[i].vehicle_model);
+        if (resposta[i].vehicle_model === this.inputVeiculo) {
+          // console.log(resposta[i].vehicle_model);
+          console.log("input-veiculo encontrado");
+          break;
+        }
+        else {
+          console.log("input-veiculo não encontrado");
+        }
+      }
+      // this.verificarInput();
+      // if (resposta.vehicle_model === this.inputVeiculo) {
+      //   console.log("input-veiculo encontrado");
+      // }else {
+      //   console.log("input-veiculo não encontrado");
+      // }
+      
+      
+      // Verifica se o inputVeiculo está presente na resposta
+      
     }
     
-    )
-  
+
+    ).catch((error) => {
+      console.error('Erro ao buscar dados:', error);
+    })
+    // this.verificarInput();
   }
   respostaveiculo!: any;
-  // inputVeiculo!: HTMLInputElement;
-  inputVeiculo: HTMLElement = document.getElementById("input-veiculo") as HTMLElement;
-  
-  if (this.inputVeiculo) {
-    inputVeiculo.addEventListener("input", function() {
-      console.log(inputVeiculo.value);
-    });
+  // resposta = this.respostaveiculo;
+  verificarInput() {
+    if (this.respostaveiculo.vehicle_model === this.inputVeiculo) {
+      console.log("input-veiculo encontrado");
+    }else {
+      console.log("input-veiculo não encontrado");
+    }
   }
-  
 }
+
+
+
+// verificarInput2(){
+//   this.inputVeiculo.addEventListener("input", function() {
+//   console.log(this.inputVeiculo.value);
+// });
+// }
+
+
+// inputVeiculo.addEventListener("input", function() {
+
+
+
+
+// inputVeiculo!: HTMLInputElement;
+// input-Veiculo: HTMLInputElement = document.getElementById("input-veiculo") as HTMLElement;
+
+// if (this.inputVeiculo) {
+//   inputVeiculo.addEventListener("input", function() {
+//   console.log(inputVeiculo.value);
+//   });
+// }
